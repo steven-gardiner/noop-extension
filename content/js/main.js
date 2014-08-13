b6bb0ccb39bcd30e71266c83e8a1f80854f544c6
steven-gardiner <steven_gardiner@yahoo.com>
2014-08-13 15:06:41 -0400

    deps and locale

--- content/js/main.js
+++ content/js/main.js
@@ -1,7 +1,10 @@
+require("/locale/noopStrings.js");
+
 //env.event_prefix = "noop-";
 require("/content/lib/jquery-2.1.1.min.js");
+require("/content/js/firefox-component.js");
 require("/content/js/firefox-button.js");
 require("/content/js/firefox-contextitem.js");
-require("/content/js/firefox-pageload.js");
+require("/content/js/firefox-pagewatcher.js");
 
 require('/content/js/noop.js');
