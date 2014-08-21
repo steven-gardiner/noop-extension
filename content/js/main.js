f130261250016e9bf73c6bb40716cfa990495d4c
steven-gardiner <steven_gardiner@yahoo.com>
2014-08-21 14:33:23 -0400

    urlbar

--- content/js/main.js
+++ content/js/main.js
@@ -6,5 +6,6 @@ require("/content/js/firefox-component.js");
 require("/content/js/firefox-button.js");
 require("/content/js/firefox-contextitem.js");
 require("/content/js/firefox-pagewatcher.js");
+require("/content/js/firefox-urlbaricon.js");
 
 require('/content/js/noop.js');
